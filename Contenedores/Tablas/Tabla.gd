da1e8acde75ad765e7e196872ff111e3d49a316c
Matias Barboza <matiasbarboza540@gmail.com>
2023-11-19 22:35:13 -0300

    Pantalla docente casi terminada

---
+++ Contenedores/Tablas/Tabla.gd
@@ -0,0 +1,72 @@
+extends Control
+
+
+signal data_recibida
+
+
+export var ruta_fila = ""
+
+
+onready var hijos_tabla = $VBoxContainer/PanelContainer2/ScrollContainer/VBoxContainer
+
+
+var data = []
+var numero_columnas = 0
+var fila
+
+
+onready var tabla = get_node("VBoxContainer/PanelContainer2/ScrollContainer/VBoxContainer")
+
+
+func _ready():
+	fila = load(ruta_fila)
+	connect("data_recibida", self, "data_a_completar")
+
+
+func crear_filas():
+	
+	#Crea tantas filas como el tamaño del array de data
+	for i in range(0, data.size()):
+		var nueva_fila = fila.instance()
+		nueva_fila.name = str(i)
+		
+		tabla.add_child(nueva_fila)
+
+
+func rellenar_tabla():
+	
+	#Obtiene numero de columnas en base a la cantidad de labels que ofician de las mismas
+	numero_columnas = obtener_numero_columnas()
+	
+	for i in range(0, data.size()):
+		fila = tabla.get_child(i)
+		
+		for j in range(0, numero_columnas):
+			fila.get_node("VBoxContainer/HBoxContainer").get_child(j).text = str(data[i][j])
+
+
+func reiniciar_tabla():
+	var hijos = hijos_tabla.get_child_count()
+	var i = 0
+	if hijos > 0:
+		while i < hijos:
+			hijos_tabla.get_child(i).queue_free()
+			i += 1
+
+
+func obtener_numero_columnas():
+	
+	return get_node("VBoxContainer/PanelContainer/HBoxContainer").get_child_count()
+
+
+func set_data(data_externa:Array):
+	
+	data = data_externa
+	fila = load(ruta_fila)
+	emit_signal("data_recibida")
+
+
+func data_a_completar():
+	
+	crear_filas()
+	rellenar_tabla()
--- Pantalla/PantallaDocente.gd
+++ Pantalla/PantallaDocente.gd
@@ -2,14 +2,31 @@ extends Control
 
 
 var desplegado : bool
+var paneles : Array
 
 
 onready var animation_player : AnimationPlayer = $AnimationPlayer
+onready var panel_bienvenida : Panel = $Bienvenida/PanelBienvenida
+onready var panel_horarios : Panel = $TablaHorarios/PanelHorarios
+onready var panel_cursos : Panel = $TablaCursos/PanelCursos
+onready var panel_carga_notas : Panel = $CargarNotas/PanelCargaNotas
 
 
 func _ready() -> void:
 	
 	desplegado = false
+	
+	paneles = [panel_bienvenida, panel_horarios, panel_carga_notas, panel_cursos]
+
+
+func activar_panel(panel_a_visibilizar : Panel):
+	for panel in paneles:
+		if panel != panel_a_visibilizar:
+			panel.visible = false
+		else:
+			panel.visible = true
+	animation_player.play("replegar")
+	desplegado = not desplegado
 
 
 func _on_ButtonMenuLateral_pressed() -> void:
@@ -20,3 +37,32 @@ func _on_ButtonMenuLateral_pressed() -> void:
 		animation_player.play("replegar")
 	
 	desplegado = !desplegado
+
+
+func _on_ButtonHorarios_pressed() -> void:
+	
+	activar_panel(panel_horarios)
+
+
+func _on_ButtonCargarNotas_pressed() -> void:
+	
+	activar_panel(panel_carga_notas)
+
+
+func _on_ButtonCursos_pressed() -> void:
+	
+	activar_panel(panel_cursos)
+
+
+func _on_ButtonCombinado_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_ButtonCN_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+
+
+
